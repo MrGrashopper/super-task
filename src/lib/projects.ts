6849abdb313b7e7a2d606db47a655ef482a506c6
Daniel Mikolai <d.mikolai@gmx.de>
2025-05-26 00:13:27 +0200

    fix

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,47 +1,9 @@
 import { prisma } from "@lib/prisma";
-import type { Project, Task, Subtask } from "@lib/types";
+import type { Project } from "@lib/types";
 
 export const getProjects = async (): Promise<Project[]> => {
   const raw = await prisma.project.findMany({
     include: { tasks: { include: { subtasks: true } } },
   });
-  return raw.map<Project>(
-    ({ id, title, description, dueDate, status, tasks }) => ({
-      id,
-      title,
-      description: description ?? undefined,
-      dueDate: dueDate.toISOString(),
-      status,
-      tasks: tasks.map<Task>(
-        ({
-          id,
-          title,
-          description,
-          dueDate,
-          status,
-          order,
-          projectId,
-          subtasks,
-        }) => ({
-          id,
-          title,
-          description: description ?? undefined,
-          dueDate: dueDate.toISOString(),
-          status,
-          order,
-          projectId,
-          subtasks: subtasks.map<Subtask>(
-            ({ id, title, description, dueDate, status, taskId }) => ({
-              id,
-              title,
-              description: description ?? undefined,
-              dueDate: dueDate.toISOString(),
-              status,
-              taskId,
-            })
-          ),
-        })
-      ),
-    })
-  );
+  return JSON.parse(JSON.stringify(raw)) as Project[];
 };
